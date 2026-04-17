d004a55e6b59bb4ae8164362fc81b18825f6e7d5
MUHAMMAD MUAZ <maaznadeem4433@gmail.com>
2026-04-17 13:42:28 +0500

    Lab 5 Completed by Muhammad Muaz - Warehouse Robot AI (UCS State Space Search)

--- src/heuristics.py
+++
@@ -1,5 +0,0 @@
-import math
-def heuristic_zero(n, g): return 0.0
-def heuristic_manhattan(n, g): return abs(n[0]-g[0]) + abs(n[1]-g[1])
-def heuristic_euclidean(n, g): return math.hypot(n[0]-g[0], n[1]-g[1])
-HEURISTICS = {'zero': heuristic_zero, 'manhattan': heuristic_manhattan, 'euclidean': heuristic_euclidean}
